c37208f6fe78903605e3288a5b67be22ecd9114a
keshavdulal <keshav.dulal@gmail.com>
2020-08-20 19:41:30 +0545

    master : clearing gatsby defaults & sass setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h5>Hi, I am</h5>
+    <h1>Keshav Dulal</h1>
+    <h3>Frontend Software Engineer</h3>
+    <p>I build things for web</p>
+    {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
   </Layout>
 )
 
