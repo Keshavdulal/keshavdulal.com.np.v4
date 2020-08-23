e2bfb3a4eeff8d1b01dc14672d384b58624f9bb9
keshavdulal <keshav.dulal@gmail.com>
2020-08-23 16:25:51 +0545

    Dynamically route data to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,26 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import portfolio from "../portfolio";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h5>Hi, I am</h5>
-    <h1>Keshav Dulal</h1>
-    <h3>Frontend Software Engineer</h3>
-    <p>I build things for web</p>
+    <h1>{portfolio.bio.firstName + ` ` + portfolio.bio.lastName}</h1>
+    <h2>{portfolio.bio.slogan}</h2>
+    <div>
+      {portfolio.bio.description.map((d) => (
+        <p>{d}</p>
+      ))}
+      <p>Here are a few technologies I've been working with recently:</p>
+    </div>
+    <ul>
+      {portfolio.skillset.core.map((i) => (
+        <li>{i}</li>
+      ))}
+    </ul>
+    <span>Get in Touch</span>
   </Layout>
 );
 
