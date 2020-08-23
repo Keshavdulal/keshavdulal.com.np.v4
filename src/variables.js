d1230f9d818e13b91ac4f94804b11315049678a7
keshavdulal <keshav.dulal@gmail.com>
2020-08-23 16:24:12 +0545

    rename file

--- src/variables.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  socialMedia: {
-    github: "https://github.com/keshavdulal",
-    stackOverFlow: "https://stackoverflow.com/users/3556531/keshavdulal",
-    linkedIn: "https://www.linkedin.com/in/keshavdulal/",
-    twitter: "https://twitter.com/keshavdulal",
-    // instagram:'',
-  },
-};
