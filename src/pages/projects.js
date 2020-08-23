754def903f33c09c325bc5817abc0b3c916695b1
keshavdulal <keshav.dulal@gmail.com>
2020-08-23 17:27:49 +0545

    basic content mgmt

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,34 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
 // import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import { portfolio } from "../portfolio";
 
 const Projects = () => (
   <Layout>
     <SEO title="projects" />
     <h1>Projects</h1>
+    <div>
+      {portfolio.petProjects.map((project) => (
+        <li>
+          <h2>{project.name}</h2>
+          <p>{project.descrption}</p>
+          <p>
+            {project.technologies.map((tech) => (
+              <span>{tech}</span>
+            ))}
+          </p>
+          <a target="_blank" href={project.github}>
+            Github
+          </a>
+          <a target="_blank" href={project.url}>
+            Demo
+          </a>
+        </li>
+      ))}
+    </div>
   </Layout>
 );
 
